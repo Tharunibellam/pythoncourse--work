b6e86fd16f20b9a5a7c805a5cb64a5292c8cfbae
Bellam-412 <169157987+Bellam-412@users.noreply.github.com>
2025-07-02 15:25:39 +0530

    myvar

--- Classwork/4.pythonoperations.py
+++ Classwork/4.pythonoperations.py
@@ -12,12 +12,14 @@ print("Exponentiation(**):",a**b) #Exponentiation(**): 100000000000000000000
 
 #2.Comparison opertors
 
-print("Equal to(==):",a==b)
-print("Not Equal to(!=):",a!=b)
-print("Greater than (>):",a>b)
-print("Less than (<):",a<b)
-print("Greater than Equal to(>=):",a>=b)
-print("Less than or Equal to(<=):",a<=b)
+print("Equal to(==):",a==b)   #Equal to(==): False
+print("Not Equal to(!=):",a!=b)  #Not Equal to(!=): True
+print("Greater than (>):",a>b)   #Greater than (>): False
+print("Less than (<):",a<b)     #Less than (<): True
+print("Greater than Equal to(>=):",a>=b)  #Greater than Equal to(>=): False
+print("Less than or Equal to(<=):",a<=b)  #Less than or Equal to(<=): True
 
       
+
+#3.Assignment Operators
 
