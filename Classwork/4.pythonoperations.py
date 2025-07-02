79b814610311f9968307e6895ce527e3d8f8a20d
Bellam-412 <169157987+Bellam-412@users.noreply.github.com>
2025-07-02 11:29:33 +0530

    Pythonopertions

---
+++ Classwork/4.pythonoperations.py
@@ -0,0 +1,11 @@
+#1.Arithmetic operators
+a=10
+b=20
+print("Addition(+):",a+b)
+print("Subtraction(-):",a-b)
+print("Multication(*):",a*b)
+print("Division(/):",a%b)
+print("Floor Division(//):",a//b)
+print("Modulus(%):",a%b)
+print("Exponentiation(**):",a**b)
+
