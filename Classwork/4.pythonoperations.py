1baa2426a21ee7918438cff06371d82b83320b1f
Bellam-412 <169157987+Bellam-412@users.noreply.github.com>
2025-07-02 11:45:18 +0530

    Pythonopertions

--- Classwork/4.pythonoperations.py
+++ Classwork/4.pythonoperations.py
@@ -1,11 +1,23 @@
 #1.Arithmetic operators
 a=10
 b=20
-print("Addition(+):",a+b)
-print("Subtraction(-):",a-b)
-print("Multication(*):",a*b)
-print("Division(/):",a%b)
-print("Floor Division(//):",a//b)
-print("Modulus(%):",a%b)
-print("Exponentiation(**):",a**b)
+print("Addition(+):",a+b)       # Addition(+): 30
+print("Subtraction(-):",a-b)  # Subtraction(-): -10
+print("Multication(*):",a*b)  # Multication(*): 200
+print("Division(/):",a%b)     # Division(/): 10
+print("Floor Division(//):",a//b)  #Floor Division(//): 0
+print("Modulus(%):",a%b)          #Modulus(%): 10
+print("Exponentiation(**):",a**b) #Exponentiation(**): 100000000000000000000
+
+
+#2.Comparison opertors
+
+print("Equal to(==):",a==b)
+print("Not Equal to(!=):",a!=b)
+print("Greater than (>):",a>b)
+print("Less than (<):",a<b)
+print("Greater than Equal to(>=):",a>=b)
+print("Less than or Equal to(<=):",a<=b)
+
+      
 
